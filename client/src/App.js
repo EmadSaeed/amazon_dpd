149e428071da0f21a442478cc27b2ddf6683c8e7
Emad Saeed <hello@emadsaeed.com>
2021-09-07 17:59:51 +0100

    initialise dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+import logo from './AZ-DPD-logo.png';
+
+import FirstPage from "./components/pages/firstPage/FirstPage";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Shipments from "./components/pages/shipments/Shipments";
+import DeliveryNotes from "./components/pages/deliveryNotes/DeliveryNotes";
+import AddressBook from "./components/pages/addressBook/AddressBook";
+import AddressForm from "./components/pages/addressBook/addressForm/AddressForm";
+import ReviewShipping from "./components/pages/reviewShipping/ReviewShipping";
+import ShipperAddress from "./components/pages/shipperAddress/ShipperAddress";
+import { useState } from "react";
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  // const now = new Date();
+  // const [dateCreated, setDateCreated] = useState(dateformat(now, "yyyy-mm-dd"));
+  const [dateCreated, setDateCreated] = useState("2020-03-01");
+  const [purchaseOrderId, setPurchaseOrderId] = useState();
+  const [deliveryNoteSubmitted, setDeliveryNoteSubmitted] = useState(0)
+
+  console.log("dateCreated: ", dateCreated);
+
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //     dispatch(getAddresses(1));
+  //     // dispatch(getShipments(dateformat(dateCreated, "dd/mm/yyyy")));
+  //     // dispatch(getBoxes());
+  // }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='firsPageMobile'>
+        <img className="firsPageAZ-logo" src={logo} alt="amazon/dpd logo"/>
+        <h1>amazon-dpd Shipping Builder</h1>
+        <span className='tryMe'>Try me on desktop!</span>
+      </div>
+      <Topbar />
+      <div className="AppContainer">
+        <Sidebar purchaseOrderId={purchaseOrderId} dateCreated={dateCreated} />
+        <div className="pagesContainer">
+          <Switch>
+            <Route exact path="/">
+              <FirstPage />
+            </Route>
+            <Route path="/fulfilment-address">
+              <AddressBook currentId={currentId} setCurrentId={setCurrentId} />
+            </Route>
+            <Route path="/select-shipment">
+              <Shipments
+                dateCreated={dateCreated}
+                setDateCreated={setDateCreated}
+                purchaseOrderId={purchaseOrderId}
+                setPurchaseOrderId={setPurchaseOrderId}
+                setDeliveryNoteSubmitted={setDeliveryNoteSubmitted}
+              />
+            </Route>
+            <Route path="/delivery-notes">
+              <DeliveryNotes setDeliveryNoteSubmitted={setDeliveryNoteSubmitted} />
+            </Route>
+            <Route path="/invoice-address">
+              <AddressBook currentId={currentId} setCurrentId={setCurrentId} />
+            </Route>
+            <Route path="/update-fulfilment-address">
+              <AddressForm currentId={currentId} setCurrentId={setCurrentId} />
+            </Route>
+            <Route path="/update-invoice-address">
+              <AddressForm currentId={currentId} setCurrentId={setCurrentId} />
+            </Route>
+            <Route path="/new-fulfilment-address">
+              <AddressForm />
+            </Route>
+            <Route path="/new-invoice-address">
+              <AddressForm />
+            </Route>
+            <Route path="/review-shipping">
+              <ReviewShipping
+                purchaseOrderId={purchaseOrderId}
+                dateCreated={dateCreated}
+                deliveryNoteSubmitted={deliveryNoteSubmitted}
+              />
+            </Route>
+            <Route path="/edit-shipper-address">
+              <ShipperAddress />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './redux/reducers';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
